a01b8f14c058f15cf2f5efe72d9f9eab0a2de182
vupmalis <vupmalis@gmail.com>
2025-11-29 21:24:55 +0200

    feat: Add spring boot init project

---
+++ src/main/java/org/mydevnotes/stock_tracker_rest/StockTrackerRestApplication.java
@@ -0,0 +1,13 @@
+package org.mydevnotes.stock_tracker_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackerRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mydevnotes/stock_tracker_rest/StockTrackerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mydevnotes.stock_tracker_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
